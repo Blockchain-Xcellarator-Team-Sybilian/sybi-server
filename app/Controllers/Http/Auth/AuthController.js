4b915b63f65e1b400ee03952218eb5c24af3ec66
Alexander Paul P. Quinit <paulquinit@gmail.com>
2019-11-01 19:14:45 +0800

    Fix indention

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -5,61 +5,59 @@ const User = use('App/Models/User');
 const UnauthorizedLoginException = use('App/Exceptions/UnauthorizedLoginException')
 
 class AuthController {
+  async register({ request, response }) {
+    // Log request
+    Logger.info('Register user request', { request })
 
-    async register({ request, response }) {
-        // Log request
-        Logger.info('Register user request', { request })
+    // Get request body
+    const requestBody = request.only(['username', 'password', 'type'])
 
-        // Get request body
-        const requestBody = request.only(['username', 'password', 'type'])
+    // Instantiate a new User object
+    let user = new User()
+    user.username = requestBody.username
+    user.password = requestBody.password
+    user.type = requestBody.type
 
-        // Instantiate a new User object
-        let user = new User()
-        user.username = requestBody.username
-        user.password = requestBody.password
-        user.type = requestBody.type
+    // Save user to database
+    await user.save()
 
-        // Save user to database
-        await user.save()
+    const responseStatus = 200
+    const responseCode = 'SUCCESS_USER_REGISTERED'
+    const responseData = { user }
+
+    return response.status(responseStatus).json({
+      code: responseCode,
+      data: responseData
+    })
+  }
+
+  async login({ request, auth, response }) {
+    // Log request
+    Logger.info('Login user request', { request })
+
+    // Get request body
+    const requestBody = request.only(['username', 'password'])
+
+    try {
+      // Validate user credentials 
+      if (await auth.attempt(requestBody.username, requestBody.password)) {
+        // Get user from database
+        let user = await User.findBy('username', requestBody.username)
+        let token = await auth.generate(user)
 
         const responseStatus = 200
-        const responseCode = 'SUCCESS_USER_REGISTERED'
-        const responseData = { user }
+        const responseCode = 'SUCCESS_USER_LOGGED_IN'
+        const responseData = { token }
 
         return response.status(responseStatus).json({
           code: responseCode,
           data: responseData
         })
+      }
+    } catch (e) {
+      throw new UnauthorizedLoginException()
     }
-
-    async login({ request, auth, response }) {
-        // Log request
-        Logger.info('Login user request', { request })
-
-        // Get request body
-        const requestBody = request.only(['username', 'password'])
-
-        try {
-          // Validate user credentials 
-          if (await auth.attempt(requestBody.username, requestBody.password)) {
-            // Get user from database
-            let user = await User.findBy('username', requestBody.username)
-            let token = await auth.generate(user)
-
-            const responseStatus = 200
-            const responseCode = 'SUCCESS_USER_LOGGED_IN'
-            const responseData = { token }
-
-            return response.status(responseStatus).json({
-              code: responseCode,
-              data: responseData
-            })
-          }
-        } catch (e) {
-          throw new UnauthorizedLoginException()
-        }
-    }
-
+  }
 }
 
 module.exports = AuthController
