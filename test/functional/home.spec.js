ceb1ee3e8cd03d7bfc0d8ccc8fcdbafb42ca9816
Alexander Paul P. Quinit <paulquinit@gmail.com>
2019-11-01 12:03:18 +0800

    Update test message

--- test/functional/home.spec.js
+++ test/functional/home.spec.js
@@ -4,7 +4,7 @@ const { test, trait } = use('Test/Suite')('Home')
 
 trait('Test/ApiClient')
 
-test('get application name and version', async ({ client }) => {
+test('should return application name and version', async ({ client }) => {
   const response = await client.get('/').end()
 
   response.assertStatus(200)
