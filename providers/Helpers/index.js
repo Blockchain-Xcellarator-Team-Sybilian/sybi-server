97d7004d53b747bd35820b764331e076e908accd
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-11-06 13:33:22 +0800

    Add default value to response data

--- providers/Helpers/index.js
+++ providers/Helpers/index.js
@@ -3,7 +3,7 @@
 // Create global helper functions here
 
 class Helpers {
-  async formatResponse (response, responseStatus, responseCode, responseData) {
+  async formatResponse (response, responseStatus, responseCode, responseData = null) {
     return await response.status(responseStatus).json({
       code: responseCode,
       data: responseData
