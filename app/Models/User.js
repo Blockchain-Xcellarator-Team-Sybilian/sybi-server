42f165f4b82d120dee06c5b8cf5aab354f624ed6
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-11-07 10:45:30 +0800

    Update user model

--- app/Models/User.js
+++ app/Models/User.js
@@ -6,24 +6,9 @@ const Model = use('Model')
 class User extends Model {
   static boot () {
     super.boot()
-
-    /**
-     * A hook to hash the user password before saving
-     * it to the database.
-     */
     this.addHook('beforeSave', 'UserHook.hashPassword')
   }
 
-  /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
-   */
   tokens () {
     return this.hasMany('App/Models/Token')
   }
