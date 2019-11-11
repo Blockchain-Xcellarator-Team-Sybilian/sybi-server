bcc099b6e0208d73d562c5bf6ec475494d5e4786
Alexander Paul Quinit <NOC-1044@neworientalclub88.local>
2019-11-11 17:14:25 +0800

    Create document helper

--- app/Helpers/DocumentHelper.js
+++ app/Helpers/DocumentHelper.js
@@ -1,24 +1,26 @@
 'use strict'
 
-const PDF = use('PDF')
 const fs = require('fs');
 const Helpers = use('Helpers')
+const PDFDocument = require('pdfkit')
 
 class DocumentHelper {
   constructor (generator) {
     this.generator = generator
   }
 
-  async generatePDF (loanCode) {
+  async generatePDF (loanCode, content) {
+    const document = new PDFDocument;
     const documentName = await this.generator.code(6) + '.pdf'
     const documentDirectory = Helpers.tmpPath('loans/') + loanCode + '/PDF/'
     
-    const content = [
-      { text: 'test' }
-    ]
-    const stream = await fs.createWriteStream(documentDirectory + documentName)
-
-    await PDF.create(content, stream)
+    document.pipe(fs.createWriteStream(documentDirectory + documentName))
+    document.fontSize(8)
+    document.text(content, {
+      width: 410,
+      align: 'left'
+    })
+    document.end()
   }
 }
   
